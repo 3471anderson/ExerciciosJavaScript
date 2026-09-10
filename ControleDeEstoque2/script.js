8aa70c137b484e990e99ecd6e91210d0ee1a53ab
Anderson Gomes <3471anderson.gomes@gmail.com>
2026-09-10 12:05:56 -0300

    DolarReal

--- ControleDeEstoque2/script.js
+++ ControleDeEstoque2/script.js
@@ -1,58 +1,68 @@
 let totalData = 0;
 
-function addItems(){
-    
-    let itemName = document.querySelector("#itemName");
-    let totalQuantity = document.querySelector("#totalQuantity");
-    let itemPrice = document.querySelector("#itemPrice");
-    let itemCategory = document.querySelector("#itemCategory");
+function addItems() {
+  let itemName = document.querySelector("#itemName");
+  let totalQuantity = document.querySelector("#totalQuantity");
+  let itemPrice = document.querySelector("#itemPrice");
+  let itemCategory = document.querySelector("#itemCategory");
+  let itemCurrency = document.querySelector("#itemCurrency");
 
-    let itemData = document.querySelector("#itemData");
+  let itemData = document.querySelector("#itemData");
 
-    // console.log(itemName.value, totalQuantity.value, itemPrice.value, itemCategory.value)
+  // console.log(itemName.value, totalQuantity.value, itemPrice.value, itemCategory.value)
 
-    if(itemName.value === "" || totalQuantity.value === "" || itemPrice.value === "" || itemCategory.value === ""){
-        alert("Por favor, preencha todos os campos!")
-    }
-    else{
-        let row = itemData.insertRow();
-        let data1 = row.insertCell(0);
-        let data2 = row.insertCell(1);
-        let data3 = row.insertCell(2);
-        let data4 = row.insertCell(3);
-        let data5 = row.insertCell(4);
-        let data6 = row.insertCell(5);
+  if (
+    itemName.value === "" ||
+    totalQuantity.value === "" ||
+    itemPrice.value === "" ||
+    itemCategory.value === ""
+  ) {
+    alert("Por favor, preencha todos os campos!");
+  } else {
+    let row = itemData.insertRow();
+    let data1 = row.insertCell(0);
+    let data2 = row.insertCell(1);
+    let data3 = row.insertCell(2);
+    let data4 = row.insertCell(3);
+    let data5 = row.insertCell(4);
+    let data6 = row.insertCell(5);
 
-        totalData++;
-        let deleteBtn = `<button class='btn btn-danger' id='deletar_${totalData}' onclick='deleteItem(this)'>Deletar</button>`
-        // totalData = totalData + 1;
+    totalData++;
+    let deleteBtn = `<button class='btn btn-danger' id='deletar_${totalData}' onclick='deleteItem(this)'>Deletar</button>`;
+    // totalData = totalData + 1;
 
-        data1.innerHTML = totalData;
-        data2.innerHTML = itemName.value;
-        data3.innerHTML = totalQuantity.value;
-        data4.innerHTML = itemPrice.value;
-        data5.innerHTML = itemCategory.value;
-        data6.innerHTML = deleteBtn;
+    let moeda = itemCurrency.value;
+    let locale = moeda === "BRL" ? "pt-BR" : "en-US";
 
-        itemName.value = "",
-        totalQuantity.value = "",
-        itemPrice.value = "",
-        itemCategory.value = ""
+    let precoFormatado = new Intl.NumberFormat(locale, {
+      style: "currency",
+      currency: moeda,
+    }).format(parseFloat(itemPrice.value));
 
-    }
+    data1.innerHTML = totalData;
+    data2.innerHTML = itemName.value;
+    data3.innerHTML = totalQuantity.value;
+    data4.innerHTML = precoFormatado;
+    data5.innerHTML = itemCategory.value;
+    data6.innerHTML = deleteBtn;
+
+    ((itemName.value = ""),
+      (totalQuantity.value = ""),
+      (itemPrice.value = ""),
+      (itemCategory.value = ""));
+  }
 }
 
-function deleteItem(e){
-    //console.log(e);
-    //console.log(e.parentElement);
+function deleteItem(e) {
+  //console.log(e);
+  //console.log(e.parentElement);
 
-    let rowElem = (e.parentElement).parentElement;
-    //console.log(rowElem)
+  let rowElem = e.parentElement.parentElement;
+  //console.log(rowElem)
 
-    if(window.confirm("Tem certeza que quer deletar isso?")){
-        rowElem.style.display = "none"; 
-    }
-    else{
-        alert("Tudo certo");
-    }
+  if (window.confirm("Tem certeza que quer deletar isso?")) {
+    rowElem.style.display = "none";
+  } else {
+    alert("Tudo certo");
+  }
 }
